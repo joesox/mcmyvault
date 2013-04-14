5c9b6de7f4f5a05e26ff14e8a631a49db11d6239
joesox <joesox@0dcacc4c-d274-b447-0539-c62a5550a6b7>
2013-04-14 01:14:03 +0000

    v1.5.1.8 prep

--- trunk/MCMyVault/Inno/jpsiii_innobuildprep.bat
+++
@@ -1,8 +0,0 @@
-DEL /q "C:\Users\Joe\Documents\MCMyVault\backups"
-DEL /q "C:\Users\Joe\Documents\MCMyVault\backups-texturepacks"
-DEL /q "C:\Users\Joe\Documents\MCMyVault\mcmapDZ\renders"
-DEL /q "C:\Users\Joe\Documents\MCMyVault\mcmapDZ_latest-32bit\mcmapDZ\renders"
-DEL /q "C:\Users\Joe\Documents\MCMyVault\snapshots"
-
-
-PAUSE
